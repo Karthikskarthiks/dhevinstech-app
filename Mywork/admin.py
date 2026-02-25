1594f89774bcb073089c7624586e1a096e478bff
Karthick S <karthikskarthiks888@gmail.com>
2026-02-25 23:10:51 +0530

    Initial commit

--- Mywork/admin.py
+++ Mywork/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
+from .models import Labour, Site, WorkDetail, Vendor
 
-# Register your models here.
-from django.contrib import admin
-from .models import Labour, Site, WorkDetail,Vendor
+@admin.register(WorkDetail)
+class WorkDetailAdmin(admin.ModelAdmin):
+    list_display = ['date', 'vendor', 'site']
+    filter_horizontal = ['labours']  # safer ManyToMany handling
 
 admin.site.register(Labour)
 admin.site.register(Vendor)
 admin.site.register(Site)
-admin.site.register(WorkDetail)
-
